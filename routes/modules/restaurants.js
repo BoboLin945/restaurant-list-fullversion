82a0ce4dcce0a2f60fe222b9bbafd1863598dfe0
BoboLin <bobolincowo@gmail.com>
2021-07-04 12:20:40 +0800

    modify search and sort function

--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -11,9 +11,10 @@ router.get('/new', (req, res) => {
 
 // Search
 router.get('/search', (req, res) => {
+  const userId = req.user._id
   const keyword = req.query.keyword
   const regex = new RegExp(keyword, 'i') // i for case insensitive
-  Restaurant.find({ $or: [{ name: { $regex: regex } }, { category: { $regex: regex } }] })
+  Restaurant.find({ userId, $or: [{ name: { $regex: regex } }, { category: { $regex: regex } }] })
     .lean()
     .then(restaurants => res.render('index', { restaurants }))
     .catch(error => console.error(error))
@@ -24,13 +25,13 @@ router.post('/', (req, res) => {
   const userId = req.user._id
   const { name, category, image, location, phone, google_map, rating, description } = req.body
   return Restaurant.create({
-    name, 
-    category, 
-    image, 
-    location, 
-    phone, 
-    google_map, 
-    rating, 
+    name,
+    category,
+    image,
+    location,
+    phone,
+    google_map,
+    rating,
     description,
     userId
   })
@@ -42,7 +43,7 @@ router.post('/', (req, res) => {
 router.get('/:id', (req, res) => {
   const userId = req.user._id
   const _id = req.params.id
-  return Restaurant.findOne({ _id, userId})
+  return Restaurant.findOne({ _id, userId })
     .lean()
     .then((restaurant) => res.render('detail', { restaurant }))
     .catch(error => console.log(error))
@@ -61,18 +62,18 @@ router.get('/:id/edit', (req, res) => {
 router.put('/:id', (req, res) => {
   const userId = req.user._id
   const _id = req.params.id
-  const { name, category, image, location, phone, google_map, rating, description} = req.body
+  const { name, category, image, location, phone, google_map, rating, description } = req.body
   return Restaurant.findOne({ _id, userId })
     .then(restaurant => {
       restaurant.name = name,
-      restaurant.category = category,
-      restaurant.image = image,
-      restaurant.location = location,
-      restaurant.phone = phone,
-      restaurant.google_map = google_map,
-      restaurant.rating = rating,
-      restaurant.description = description,
-      restaurant.save()
+        restaurant.category = category,
+        restaurant.image = image,
+        restaurant.location = location,
+        restaurant.phone = phone,
+        restaurant.google_map = google_map,
+        restaurant.rating = rating,
+        restaurant.description = description,
+        restaurant.save()
     })
     .then(() => res.redirect(`/restaurants/${_id}`))
     .catch(error => console.log(error))
@@ -90,41 +91,28 @@ router.delete('/:id', (req, res) => {
 
 router.post('/sort', function (req, res) {
   const sortOption = req.body.sort
-  if (sortOption === '0') {
-    Restaurant.find()
-      .lean()
-      .then(restaurants => res.render('index', { restaurants, sortOption }))
-      .catch(error => console.error(error))
-  }
-  if (sortOption === '1') {
-    Restaurant.find()
-      .lean()
-      .sort({ _id: 'desc' })
-      .then(restaurants => res.render('index', { restaurants, sortOption }))
-      .catch(error => console.error(error))
-  }
-  if (sortOption === '2') {
-    Restaurant.find()
-      .lean()
-      .sort({ _id: 'asc' })
-      .then(restaurants => res.render('index', { restaurants, sortOption }))
-      .catch(error => console.error(error))
-  }
-  if (sortOption === '3') {
-    Restaurant.find()
-      .lean()
-      .sort({ category: 'asc' })
-      .then(restaurants => res.render('index', { restaurants, sortOption }))
-      .catch(error => console.error(error))
-  }
-  if (sortOption === '4') {
-    Restaurant.find()
-      .lean()
-      .sort({ rating: -1 })
-      .then(restaurants => res.render('index', { restaurants, sortOption }))
-      .catch(error => console.error(error))
-  }
+  const userId = req.user._id
+  Restaurant.find({ userId })
+    .lean()
+    .sort(getSortMethod(sortOption))
+    .then(restaurants => res.render('index', { restaurants, sortOption }))
+    .catch(error => console.log(error))
 })
+
+// sorting function
+function getSortMethod(sortOption) {
+  if (sortOption === 'desc') {
+    return { _id: 'desc' }
+  } else if (sortOption === 'asc') {
+    return { _id: 'asc' }
+  } else if (sortOption === 'category') {
+    return { category: 'asc' }
+  } else if (sortOption === 'rating') {
+    return { rating: -1 }
+  } else if (sortOption === 'default') {
+    return { _id: 'asc' }
+  }
+}
 
 // export route module
 module.exports = router
